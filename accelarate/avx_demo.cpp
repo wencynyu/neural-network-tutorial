71ef80834b2a0175647c581ce9abeac53ab30685
yuwenxin <yuwenxin980214@gmail.com>
2024-08-29 03:14:59 +0800

    correct the avx_demo to in_val * pos - in_val * nega

--- accelarate/avx_demo.cpp
+++ accelarate/avx_demo.cpp
@@ -36,19 +36,17 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < len; i += 32)
     {
         // 512 / 32 = 16
-        Zmm0 = _mm512_load_ps(pInVector + i); // zmm0: load 16 float ele
-        // 低 8 个元素复制到高 8 个位置，并生成一个包含 16 个相同值的寄存器
-        Zmm1 = _mm512_moveldup_ps(Zmm0); // zmm1: low 8 ele
-        // 高 8 个元素复制到低 8 个位置，并生成一个包含 16 个相同值的寄存器
-        Zmm2 = _mm512_movehdup_ps(Zmm0); // zmm2: high 8 ele
-        Zmm2 = _mm512_mul_ps(Zmm2, Zmm_vec_1); // zmm2 = zmm2 * zmm_vec_1
-        Zmm3 = _mm512_fmaddsub_ps(Zmm1, Zmm_vec_2, Zmm2); // zmm3: zmm1 * zmm_vec_2 - zmm2
+        // 512 / 32 = 16
+        Zmm0 = _mm512_load_ps(pInVector + i); // Load 16 floats from pInVector
+        Zmm1 = _mm512_mul_ps(Zmm0, Zmm_vec_1); // Zmm1 = in_val * vec_1
+        Zmm2 = _mm512_mul_ps(Zmm0, Zmm_vec_2); // Zmm2 = in_val * vec_2
+        Zmm3 = _mm512_sub_ps(Zmm1, Zmm2); // Zmm3 = Zmm1 - Zmm2
         _mm512_store_ps(pOutVector + i, Zmm3);
-        Zmm0 = _mm512_load_ps(pInVector + i + 16); // load 16 float ele again
-        Zmm1 = _mm512_moveldup_ps(Zmm0);
-        Zmm2 = _mm512_movehdup_ps(Zmm0);
-        Zmm2 = _mm512_mul_ps(Zmm2, Zmm_vec_1);
-        Zmm3 = _mm512_fmaddsub_ps(Zmm1, Zmm_vec_2, Zmm2);
+
+        Zmm0 = _mm512_load_ps(pInVector + i + 16); // Load next 16 floats from pInVector
+        Zmm1 = _mm512_mul_ps(Zmm0, Zmm_vec_1); // Zmm1 = in_val * vec_1
+        Zmm2 = _mm512_mul_ps(Zmm0, Zmm_vec_2); // Zmm2 = in_val * vec_2
+        Zmm3 = _mm512_sub_ps(Zmm1, Zmm2); // Zmm3 = Zmm1 - Zmm2
         _mm512_store_ps(pOutVector + i + 16, Zmm3);
     }
     std::clock_t end = std::clock();
